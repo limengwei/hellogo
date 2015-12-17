c42f9a0b805701fb633846d7954e8b016f7d2c96
lmw <892642257@qq.com>
2015-12-17 17:21:03 +0800

    add hml parser

--- hello.go
+++ hello.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"html/template"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 const (
-	PORT       = ":80"
-	UPLOAD_DIR = "./uploads"
-	TPL_DIR    = "./views"
+	PORT         = ":80"
+	UPLOAD_DIR   = "./uploads"
+	TPL_DIR      = "./views"
+	DOWNLOAD_DIR = "./downloads"
+	TimeoutLimit = 10
 )
 
 func main() {
@@ -22,6 +26,7 @@ func main() {
 	http.HandleFunc("/up", uploadHandler)
 	http.HandleFunc("/view", viewHandler)
 	http.HandleFunc("/spider", spiderHandler)
+	http.HandleFunc("/img", imgSpiderHandler)
 	err := http.ListenAndServe(PORT, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err.Error())
@@ -31,10 +36,10 @@ func main() {
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-
 	io.WriteString(w, "hello")
 }
 
+//get输出上传页面 post上传文件
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		//使用Go模板库输出
@@ -69,10 +74,13 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
+
+		//跳转到 显示图片
 		http.Redirect(w, r, "/view?id="+fileNmae, http.StatusFound)
 	}
 }
 
+//输出上传之后的文件
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fileNmae := r.FormValue("id")
 	filePath := UPLOAD_DIR + "/" + fileNmae
@@ -81,6 +89,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, filePath)
 }
 
+//请求一个链接 并输出内容
 func spiderHandler(w http.ResponseWriter, r *http.Request) {
 	res, err := http.Get("http://www.yinwang.org/")
 	if err != nil {
@@ -99,6 +108,53 @@ func spiderHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, string(body))
 }
 
+//解析HTML
+func imgSpiderHandler(w http.ResponseWriter, r *http.Request) {
+
+	doc, err := goquery.NewDocument("http://mt.locojoy.com/chengka/")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	doc.Find(".js_ulWrap_SH").Find("li").Each(func(i int, li *goquery.Selection) {
+		url, exists := li.Find("img").First().Attr("src")
+		title := li.Find("a").Eq(1).Text()
+		if !exists {
+			fmt.Println("no exists")
+		} else {
+			fmt.Println(url + "---" + title)
+
+			go download(url) //使用并发下载
+
+		}
+	})
+	io.WriteString(w, doc.Text())
+}
+
+//下载
+func download(url string) {
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println("err:", err.Error())
+		return
+	}
+	defer res.Body.Close()
+
+	fileName := filepath.Base(url)
+
+	temp, err := os.Create(DOWNLOAD_DIR + "/" + fileName)
+
+	if err != nil {
+		fmt.Println("err:", err.Error())
+		return
+	}
+
+	io.Copy(temp, res.Body)
+}
+
+//输出HTML
 func renderHtml(w http.ResponseWriter, htmlPath string) {
 	s, err := ioutil.ReadFile(htmlPath)
 	if err != nil {
